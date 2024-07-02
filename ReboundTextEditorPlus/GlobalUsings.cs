44ea4632e366f2ffb8986c283bc7255489f9f352
ErrorTek <jakub.baranski.33@gmail.com>
2024-07-02 21:44:08 +0100

    Add project files.

---
+++ ReboundTextEditorPlus/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using Microsoft.UI;
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using ReboundTextEditorPlus.Common;
+global using ReboundTextEditorPlus.Views;
+global using WinUICommunity;
+global using static ReboundTextEditorPlus.Common.AppHelper;
---
+++ ReboundTextEditorPlus/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ReboundTextEditorPlus.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+        appTitleBar.Window = App.CurrentWindow;
+    }
+}
+
